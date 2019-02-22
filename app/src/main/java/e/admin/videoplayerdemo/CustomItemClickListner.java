f4d8ac01dd1f3a9a8b17da467b8641ca42e6dd80
TATA STRIVE <ambrerushikesh999@gmail.com>
2019-02-22 11:54:18 +0530

    Initial commit

---
+++ app/src/main/java/e/admin/videoplayerdemo/CustomItemClickListner.java
@@ -0,0 +1,9 @@
+package e.admin.videoplayerdemo;
+
+import android.view.View;
+
+interface CustomItemClickListner {
+    void onItemClick(View v, int postion);
+    void onItemLongClick(int position);
+
+}
